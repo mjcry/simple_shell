c56f524a4cc68661be47215d4382e0f1ef31f6bf
mjcry <mj.masilo.jack@gmail.com>
2022-11-15 16:11:49 -0800

    SS

---
+++ simple_shell.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+ * main - Entry point of the program.
+ * @ac: The number of parameters passed to the executable file. In the case
+ * this variable will not be used.
+ * @av: The name of the program.
+ * Return: Always 0.
+ */
+int main(__attribute__((unused)) int ac, char **av)
+{
+	char *line;
+	size_t size;
+	int command_counter, GBSH_IS_INTERACTIVE;
+
+/* prints the starting screen only is shell is in interactive mode */
+	GBSH_IS_INTERACTIVE = isatty(STDIN_FILENO);
+
+	if (GBSH_IS_INTERACTIVE)
+		welcome_screen();
+
+	command_counter = 0;
+	signal(SIGINT, SIG_IGN);
+	do {
+		command_counter++;
+		line = NULL;
+		size = 0;
+		parse_line(line, size, command_counter, av);
+
+	} while (1);
+
+	return (0);
+}
